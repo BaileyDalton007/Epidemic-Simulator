9789690060076dff1ee98178da3bfe11b70958b4
Bailey Dalton <baileydalton007@gmail.com>
2022-01-13 13:45:11 -0500

    Adds node selector

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import matplotlib.pyplot as plt
+from matplotlib.backend_bases import MouseButton
 import numpy as np
 import pandas as pd
 
 population = 50
-statusList = np.zeros((population,), dtype=int)
-statusList[20] = 1
-
 np.random.seed(7)
 x, y = np.random.rand(2, population)
 
-data = np.dstack([x,y,statusList])
+statusList = np.zeros((population,), dtype=int)
 
 colormap = np.array(['b', 'r'])
 
@@ -21,7 +19,21 @@ def printTable():
 
     print(df)
 
+def on_pick(event):
+    ind = event.ind
+
+    if statusList[ind].any() != 0:
+        statusList[ind] = 0
+    else:
+        statusList[ind] = 1
+    
+    plt.clf()
+    plt.scatter(x, y, c=colormap[statusList], picker=True);
+    plt.draw()
+
 fig, ax = plt.subplots()
-ax.scatter(x, y, c=colormap[statusList])
-printTable()
+ax.scatter(x, y, c=colormap[statusList], picker=True)
+fig.canvas.mpl_connect('pick_event', on_pick)
+
 plt.show()
+plt.draw()
