c928c9999827816380aa43866927ca3a2562e784
Bailey ds <baileydalton007@gmail.com>
2022-01-18 19:06:38 -0500

    Moves common widgets to own file

--- tabs/data_tab.py
+++ tabs/data_tab.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QFont, QIntValidator
 from PyQt5.QtWidgets import QWidget, QPushButton, QGridLayout
 
 from data import DataSet
+from widgetTempletes import SliderWidget, intInputWidget
 
 class DataTab(QWidget):
     def __init__(self, parent, plotCanvas):
@@ -12,8 +13,8 @@ class DataTab(QWidget):
 
         self.plotCanvas = plotCanvas
 
-        self.sliderWidget = popSliderWidget(self)
-        self.seedWidget = seedWidget(self)
+        self.sliderWidget = SliderWidget(self, "Population")
+        self.seedWidget = intInputWidget(self, "Random Seed")
 
         self.genButton = QPushButton("Generate Population")
         self.genButton.clicked.connect(self.generate)
@@ -29,57 +30,5 @@ class DataTab(QWidget):
         self.setLayout(self.layout)
 
     def generate(self):
-        self.plotCanvas.data = DataSet(self.seedWidget.seedInput.text(), self.sliderWidget.popSlider.value())
+        self.plotCanvas.data = DataSet(self.seedWidget.intInput.text(), self.sliderWidget.value)
         self.plotCanvas.updateGraph()
-        
-
-class popSliderWidget(QWidget):
-    def __init__(self, parent):
-        super(QWidget, self).__init__(parent)
-        self.slider = QWidget
-        self.slider.layout = QtWidgets.QHBoxLayout()
-
-        self.slider.popSlider = QtWidgets.QSlider(Qt.Horizontal)
-        self.slider.popSlider.setFocusPolicy(Qt.StrongFocus)
-        self.slider.popSlider.setTickPosition(QtWidgets.QSlider.TicksBothSides)
-        self.slider.popSlider.setTickInterval(10)
-        self.slider.popSlider.setSingleStep(1)
-        self.slider.popSlider.valueChanged.connect(self.changed_slider)
-
-        self.slider.sliderlabel = QtWidgets.QLabel("00")
-        self.slider.sliderlabel.setFont(QFont("Sanserif", 10))
-
-        self.slider.layout.addWidget(self.slider.sliderlabel)
-        self.slider.layout.addWidget(self.slider.popSlider)
-
-        # Layout of title and slider + value
-        self.layout = QtWidgets.QVBoxLayout()
-
-        self.title = QtWidgets.QLabel("Population")
-        self.title.setFont(QFont("Sanserif", 12))
-
-        self.layout.addWidget(self.title)
-        self.layout.addLayout(self.slider.layout)
-
-    def changed_slider(self):
-        value = self.popSlider.value()
-        if value >= 10:
-            self.sliderlabel.setText(str(value))
-        else:
-            self.sliderlabel.setText(f'0{str(value)}')
-
-class seedWidget(QWidget):
-    def __init__(self, parent):
-        super(QWidget, self).__init__(parent)
-        self.layout = QtWidgets.QHBoxLayout()
-
-        self.seedLabel = QtWidgets.QLabel("Random Seed")
-        self.seedLabel.setFont(QFont("Sanserif", 12))
-
-        self.seedInput = QtWidgets.QLineEdit(self)
-        self.seedInput.setFixedWidth(40)
-        self.seedInput.setValidator(QIntValidator())
-
-        self.layout.addWidget(self.seedLabel)
-        self.layout.addWidget(self.seedInput)
-        self.layout.addStretch(1)
---
+++ widgetTempletes.py
@@ -0,0 +1,57 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont, QIntValidator
+from PyQt5.QtWidgets import QWidget, QPushButton
+
+
+class SliderWidget(QWidget):
+    def __init__(self, parent, title):
+        super(QWidget, self).__init__(parent)
+        self.slider = QWidget()
+        self.slider.layout = QtWidgets.QHBoxLayout()
+
+        self.slider.sliderInput = QtWidgets.QSlider(Qt.Horizontal)
+        self.slider.sliderInput.setFocusPolicy(Qt.StrongFocus)
+        self.slider.sliderInput.setTickPosition(QtWidgets.QSlider.TicksBothSides)
+        self.slider.sliderInput.setTickInterval(10)
+        self.slider.sliderInput.setSingleStep(1)
+
+        self.slider.sliderInput.valueChanged.connect(self.changed_slider)
+
+        self.slider.sliderlabel = QtWidgets.QLabel("00")
+        self.slider.sliderlabel.setFont(QFont("Sanserif", 10))
+
+        self.slider.layout.addWidget(self.slider.sliderlabel)
+        self.slider.layout.addWidget(self.slider.sliderInput)
+
+        # Layout of title and slider + value
+        self.layout = QtWidgets.QVBoxLayout()
+
+        self.title = QtWidgets.QLabel(title)
+        self.title.setFont(QFont("Sanserif", 12))
+
+        self.layout.addWidget(self.title)
+        self.layout.addLayout(self.slider.layout)
+
+    def changed_slider(self):
+        self.value = self.slider.sliderInput.value()
+        if self.value >= 10:
+            self.slider.sliderlabel.setText(str(self.value))
+        else:
+            self.slider.sliderlabel.setText(f'0{str(self.value)}')
+
+class intInputWidget(QWidget):
+    def __init__(self, parent, title):
+        super(QWidget, self).__init__(parent)
+        self.layout = QtWidgets.QHBoxLayout()
+
+        self.intLabel = QtWidgets.QLabel(title)
+        self.intLabel.setFont(QFont("Sanserif", 12))
+
+        self.intInput = QtWidgets.QLineEdit(self)
+        self.intInput.setFixedWidth(40)
+        self.intInput.setValidator(QIntValidator())
+
+        self.layout.addWidget(self.intLabel)
+        self.layout.addWidget(self.intInput)
+        self.layout.addStretch(1)
