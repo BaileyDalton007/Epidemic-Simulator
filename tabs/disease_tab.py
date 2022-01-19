e255065709c63055f0425bde127a09d438f38085
Bailey ds <baileydalton007@gmail.com>
2022-01-18 19:14:14 -0500

    Adds disease tab gui

--- tabs/disease_tab.py
+++ tabs/disease_tab.py
@@ -1,8 +1,28 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QPushButton, QGridLayout
+from PyQt5.QtGui import QFont, QIntValidator
+from PyQt5.QtWidgets import QWidget, QPushButton
+
+from widgetTempletes import SliderWidget, intInputWidget
 
 class DiseaseTab(QWidget):
     def __init__(self, parent):
         super(QWidget, self).__init__(parent)
-        self.layout = QGridLayout(self)
+        self.layout = QtWidgets.QVBoxLayout()
+
+        self.layout.addStretch(1)
+
+        self.radSlider = SliderWidget(self, "Spread Radius")
+        self.layout.addLayout(self.radSlider.layout)
+
+        self.infectionLength = intInputWidget(self, "Infection Length")
+        self.layout.addLayout(self.infectionLength.layout)
+
+        self.contSlider = SliderWidget(self, "Rate of Infection")
+        self.layout.addLayout(self.contSlider.layout)
+        
+        self.mortSlider = SliderWidget(self, "Mortality Rate")
+        self.layout.addLayout(self.mortSlider.layout)
+        self.layout.addStretch(1)
+
+        self.setLayout(self.layout)
