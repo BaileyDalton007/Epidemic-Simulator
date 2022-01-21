d0bfc9ec3775a1f0cea95e5d6a957418d8488a8b
Bailey ds <baileydalton007@gmail.com>
2022-01-21 12:26:30 -0500

    Adds simulation tab

---
+++ tabs/sim_tab.py
@@ -0,0 +1,25 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QWidget, QPushButton
+
+from widgetTempletes import SliderWidget, intInputWidget
+
+
+class SimulationTab(QWidget):
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QtWidgets.QVBoxLayout()
+
+        self.layout.addStretch(1)
+
+        self.simLength = intInputWidget(self, "Days Of Simulation")
+        self.layout.addLayout(self.simLength.layout)
+        
+        self.layout.addStretch(1)
+        
+        self.startButton = QPushButton("Start Simulation")
+        self.startButton.clicked.connect(self.startSim)
+        self.layout.addWidget(self.startButton)
+        self.setLayout(self.layout)
+
+    def startSim(self):
+        pass
